58f2af5d3ed1e3a5a14d5e044cc34e43f20fa5da
mwanyambu <jmwanyambu@gmail.com>
2023-11-24 06:59:02 -0800

    readme

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,6 @@
 #creating a file in /tmp
 file { '/tmp/school':
-  content => 'I love puppet',
+  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
