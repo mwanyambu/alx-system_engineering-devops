20c154ae3cf34b64e1ba31897dff3eb55c93e9bd
mwanyambu <jmwanyambu@gmail.com>
2023-11-26 05:49:23 -0800

    configure with puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+#seeting up using puppet
+include stdlib
+
+file_line { 'turning off passw':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Identity file':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     IdentifyFile ~/.ssh/school',
+  replace => true
+}
