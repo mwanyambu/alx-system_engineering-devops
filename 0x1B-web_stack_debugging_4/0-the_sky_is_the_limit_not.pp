195c1ebdb49822ac93a579347ceb21ba4bf65cdc
John Mwanyambu <jmwanyambu@gmail.com>
2024-01-24 07:12:35 -0800

    user limit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+#configure web server to work under pressure
+
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => /usr/local/bin/:/bin'
+}
+
+#Nginx restart
+
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => 'etc/init.d/'
+}
