79e20ae4fe2a2379f6264f157da15bcf04a4cb53
John Mwanyambu <jmwanyambu@gmail.com>
2024-01-25 12:47:51 -0800

    sky is the limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,4 @@
 #configure web server to work under pressure
 
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => /usr/local/bin/:/bin'
-}
-
-#Nginx restart
-
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => 'etc/init.d/'
-}
+exec { '/usr/bin/env sed -i s/15/1000/ etc/default/nginx': }
+-> exec { '/usr/bin/env service nginx restart': }
