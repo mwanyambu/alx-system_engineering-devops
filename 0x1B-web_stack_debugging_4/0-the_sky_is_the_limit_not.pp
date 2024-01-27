b31faaaccb1d658ffb58ab41e0409db6e42a7c37
John Mwanyambu <jmwanyambu@gmail.com>
2024-01-27 01:13:39 -0800

    exec

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,8 @@
 #configure web server to work under pressure
-exec { '/usr/bin/env sed -i s/15/1000/ etc/default/nginx': }
--> exec { '/usr/bin/env service nginx restart': }
+exec { '/usr/bin/env sed -i s/15/1000/ etc/default/nginx':
+  command => '/usr/bin/env sed -i s/15/1000/ etc/default/nginx',
+}
+exec { '/usr/bin/env service nginx restart':
+  command => '/usr/bin/env service nginx restart',
+  require => Exec['/usr/bin/env sed -i s/15/1000/ etc/default/nginx'],
+}
